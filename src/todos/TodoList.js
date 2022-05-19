d882dc74cba03438c3bac2dcb93c0ed4fed368b5
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-19 17:55:58 -0400

    added styles using tailwind

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -6,7 +6,7 @@ import TodoListItem from "./TodoListItem";
 
 // created component to map over every single item and display it in a div
 //set up an empty array to prevent eny errors
-const TodoList = ({todos = []}) => (
+const TodoList = ({todos = [{text: 'Go to the gym at 8:00 a.m.'}]}) => (
     <div className="list-wrapper">
         <NewTodoForm/>
         {todos.map(todo => <TodoListItem todo={todo}/>)}
