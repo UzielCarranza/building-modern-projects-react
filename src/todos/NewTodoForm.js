5ee8a7e8b92e71da5b843f3c1025b89a61f8fcae
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-26 17:02:00 -0400

    connecting components with the mathod connect to use Redux

--- src/todos/NewTodoForm.js
+++ src/todos/NewTodoForm.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
+import {connect} from 'react-redux';
+import {createTodo} from './actions.js'
 
-
-const NewTodoForm = ({}) => {
+const NewTodoForm = ({todos, onCreatePressed}) => {
     const [inputValue, setInputValue] = useState('');
 
-   return  (
+    return (
         <div className="new-todo-form">
             <input type="text"
                    className="new-todo-input"
@@ -12,10 +13,33 @@ const NewTodoForm = ({}) => {
                    onChange={e => setInputValue(e.target.value)}
                    placeholder="Type your new todo here"
             />
-            <button className="new-todo-button">Create Todo</button>
+            <button
+                onClick={() => {
+                    const isDuplicateText = todos.some(todo => todo.text === inputValue)
+                    if (!isDuplicateText) {
+                        onCreatePressed(inputValue);
+                        setInputValue("");
+                    }
+
+                }}
+                className="new-todo-button">Create Todo
+            </button>
         </div>
     )
 }
 
+// objects that represents properties
+// Jobs of this is to take this state object
+// and return another object containing the pieces of that state
+// that our component needs access to
+const mapStateToProps = state => ({
+    todos: state.todos,
+});
 
-export default NewTodoForm;
+//function that allows components to trigger actions that our Redux store will respond to
+// in our case, we want to trigger our Redux action when somebody clicks the create todo button
+const mapDispatchToProps = dispatch => ({
+    onCreatePressed: text => dispatch(createTodo(text))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
