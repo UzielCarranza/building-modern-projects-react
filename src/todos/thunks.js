af6edf4a45a697b18dca55ad877efa562cc96be7
UzielCarranza <uzielcarranzac97@gmail.com>
2022-07-24 20:04:30 -0400

    Adding internal server to test function with fake data... added thunks to the project to display asyncronous calls

---
+++ src/todos/thunks.js
@@ -0,0 +1,21 @@
+import {loadTodosFailure, loadTodosInProgress, loadTodoSuccess} from "./actions";
+
+export const loadTodos = () => async (dispatch, getState) => {
+
+    try {
+        dispatch(loadTodosInProgress());
+        const response = await fetch('http://localhost:8080/todos')
+        const todos = await response.json()
+
+        dispatch(loadTodoSuccess(todos))
+    }
+    catch (e){
+        dispatch(loadTodosFailure())
+        dispatch(displayAlert(e))
+    }
+
+}
+
+export const displayAlert = text => {
+    alert(text)
+}
